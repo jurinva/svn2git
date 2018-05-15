f0229dadf8a518ab6817a7fa00a01f1256858915
jurinva <jurinva@gmail.com>
2018-05-15 17:25:44 +0300

    Add confirm function

--- svn2git.sh
+++ svn2git.sh
@@ -28,8 +28,21 @@ if [ $# -gt 0 ]; then
   done
 fi
 
+confirm() {
+    # call with a prompt string or use a default
+    read -r -p "${1:-continue? [Y/n]} " response
+    case "$response" in
+        [nN])
+            true
+            ;;
+        *)
+            false
+            ;;
+    esac
+}
+
 svn checkout $SVN $TARGET
 cd $TARGET
 svn log -q | awk -F '|' '/^r/ {sub("^ ", "", $2); sub(" $", "", $2); print $2" = "$2" <"$2">"}' | sort -u > authors-transform.txt
 nano ./authors-transform.txt
-git svn clone -r$COMMIT:HEAD $SVN --no-metadata --authors-file=authors-transform.txt --stdlayout $GIT
+confirm && git svn clone -r$COMMIT:HEAD $SVN --no-metadata --authors-file=authors-transform.txt --stdlayout $GIT

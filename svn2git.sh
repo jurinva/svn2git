f3138a138fef5f4c5875f53dcf126b1cec10480a
jurinva <jurinva@gmail.com>
2018-05-25 12:05:08 +0300

    Add help

--- svn2git.sh
+++ svn2git.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+help() {
+  echo -e "\tsvn2git.sh [-s|--svn svn url] [-g|--git git folder] [-c|--commit commit] [-t|--target svn folder]\n\tsvn2git.sh [-h|--help|-v|--version]"
+  echo "Example: sv2git.sh -c 2 -s svn://svn.code.sf.net/p/sample/code/trunk -t sample-code -g sample"
+  exit
+}
+
 if [ $# -gt 0 ]; then
   while [[ $# -gt 0 ]]; do
     key="$1"
@@ -24,6 +30,9 @@ if [ $# -gt 0 ]; then
       shift # past argument
       shift # past value
       ;;
+      -h|--help)
+      help
+      ;;
     esac
   done
 fi
@@ -41,6 +50,8 @@ confirm() {
     esac
 }
 
+
+
 svn checkout $SVN $TARGET
 cd $TARGET
 svn log -q | awk -F '|' '/^r/ {sub("^ ", "", $2); sub(" $", "", $2); print $2" = "$2" <"$2">"}' | sort -u > authors-transform.txt
@@ -48,3 +59,4 @@ nano ./authors-transform.txt
 rm -rf $GIT
 if [ ! -z $COMMIT ]; then COMMITOPTN="-r$COMMIT:HEAD"; fi
 confirm && git svn clone $COMMITOPTN $SVN --no-metadata --authors-file=authors-transform.txt --stdlayout $GIT
+
